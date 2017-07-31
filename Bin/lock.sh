ce38276c768f7caff8b0a961587e7e2b9fd1d64d
dbroqua <dbroqua@mousur.org>
2017-07-31 21:57:56 +0200

    - Added notification when lock screen

--- Bin/lock.sh
+++ Bin/lock.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+notify-send --icon=gtk-info Lock "Screen lock in progress..."
+
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
 /opt/i3lock-fancy-multimonitor/lock
