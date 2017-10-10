97cded41d5e70638741d7c3e221b931020a4e32c
dbroqua <dbroqua@mousur.org>
2017-10-10 17:14:55 +0200

    - Updated list of ignored files

--- Bin/rsync.sh
+++ Bin/rsync.sh
@@ -11,7 +11,7 @@
 
 TYPE='' # usb / sshfs
 CMD=rsync
-ARGS='-avz --delete --exclude /Nextcloud --exclude /.cache --exclude /.config/google-chrome-unstable --exclude /.config/google-chrome --exclude /Downloads --exclude /.thunderbird --exclude /Projects --exclude /Software --exclude /tmp'
+ARGS='-avz --delete --exclude /Nextcloud --exclude /.cache --exclude /.config/google-chrome-unstable --exclude /.config/google-chrome --exclude /Downloads --exclude /.thunderbird --exclude /Projects --exclude /Software --exclude /tmp --exclude /VirtualBox\ VMs'
 ORIG='/home/dbroqua/'
 DEST=''
 HOSTNAME=`hostname`
